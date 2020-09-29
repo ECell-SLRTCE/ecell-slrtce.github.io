dec38fd169cfb9fc89d12111b1551e70f7bfba52
Amey Bhavsar <iamamey24@gmail.com>
2020-09-29 15:11:29 +0530

    removed unwanted temporary js and css

--- js/social-icon.js
+++ js/social-icon.js
@@ -1,12 +1,14 @@
-// the amount of scroll after which social-button will be shown
-const pxCutoff = 600,
-  fadeTime = 300;
-let socialBtn = $("#social__scroll");
-//scrolling event listener for social button
-$(window).on("scroll", function () {
-  if ($(window).scrollTop() >= pxCutoff) {
-    socialBtn.fadeIn(fadeTime);
-  } else {
-    socialBtn.fadeOut(fadeTime);
-  }
-});
+//Getting the intiail Y offset
+const initialYOffset = window.pageYOffset;
+
+//scrolling function for social button
+window.onscroll = () => {
+	//Getting current position
+	let currentPos = window.pageYOffset;
+
+	if (currentPos == initialYOffset) {
+		document.getElementById("social__scroll").style.display = "none";
+	} else {
+		document.getElementById("social__scroll").style.display = "flex";
+	}
+};
